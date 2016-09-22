075d6a8dfcf5338e92da63ee2172918042ce11b0
mightypanders <mighty.panders@gmail.com>
2016-09-22 10:54:05 +0200

    refactorings, fix walking bug, now more fluid

--- spriteanim.py
+++ spriteanim.py
@@ -12,38 +12,38 @@ class MyApp(arcade.Window):
 		self.allsprites = arcade.SpriteList()
 
 		self.player = arcade.AnimatedWalkingSprite(4.0)
-		filename = "images/SBM2-Bomberman.gif"
+		file = "images/SBM2-Bomberman.gif"
 
-		stand_loc_list = [[52, 0, 16, 28]]
-		self.player.stand_right_textures = arcade.load_textures(filename,
-		                                                        stand_loc_list,
-		                                                        False)
-		self.player.stand_left_textures = arcade.load_textures(filename,
-		                                                       stand_loc_list,
-		                                                       True)
-		walk_loc_list = [[52, 0, 16, 28],
-		                 [72, 0, 16, 28],
-		                 [52, 0, 16, 28],
-		                 [89, 0, 16, 28]]
+		stand_left_right = [[52, 00, 16, 28]]
+		self.player.stand_right_textures = arcade.load_textures(file, stand_left_right, False)
+		self.player.stand_left_textures = arcade.load_textures(file, stand_left_right, True)
+
+		stand_up = [[00, 00, 18, 28]]
+		self.player.stand_up_textures = arcade.load_textures(file, stand_up, False)
+
+		stand_down = [[105, 00, 18, 28]]
+		self.player.stand_down_textures = arcade.load_textures(file, stand_down, False)
+
+		walk_sprites = [[52, 00, 16, 28],
+		                [72, 00, 16, 28],
+		                [52, 00, 16, 28],
+		                [89, 00, 16, 28]]
 		self.player.walk_right_textures = \
-			arcade.load_textures(filename, walk_loc_list, False)
+			arcade.load_textures(file, walk_sprites, False)
 		self.player.walk_left_textures = \
-			arcade.load_textures(filename, walk_loc_list, True)
+			arcade.load_textures(file, walk_sprites, True)
 
-		walk_loc_list = [[0, 0, 18, 28],
-		                 [18, 0, 18, 28],
-		                 [0, 0, 18, 28],
-		                 [36, 0, 18, 28]]
-		self.player.walk_up_walk_textures = arcade.load_textures(filename,
-		                                                         walk_loc_list,
-		                                                         False)
-		walk_loc_list = [[105, 0, 18, 28],
-		                 [123, 0, 18, 28],
-		                 [105, 0, 18, 28],
-		                 [141, 0, 18, 28]]
-		self.player.walk_down_textures = arcade.load_textures(filename,
-		                                                      walk_loc_list,
-		                                                      False)
+		walk_sprites = [[00, 00, 18, 28],
+		                [18, 00, 18, 28],
+		                [00, 00, 18, 28],
+		                [36, 00, 18, 28]]
+		self.player.walk_up_walk_textures = arcade.load_textures(file, walk_sprites, False)
+
+		walk_sprites = [[105, 00, 18, 28],
+		                [123, 00, 18, 28],
+		                [105, 00, 18, 28],
+		                [141, 00, 18, 28]]
+		self.player.walk_down_textures = arcade.load_textures(file, walk_sprites, False)
 
 		self.player.texture_change_distance = 50
 		self.player.center_y = screen_h / 2
@@ -58,25 +58,29 @@ class MyApp(arcade.Window):
 
 	def on_key_press(self, key, modifiers):
 		"""
-		Called whenever the mouse moves.
+		Called whenever a key is pressed.
 		"""
 		if key == arcade.key.UP:
-			self.player.change_y = mov_speed
+			self.player.change_y += mov_speed
 		elif key == arcade.key.DOWN:
-			self.player.change_y = -mov_speed
+			self.player.change_y -= mov_speed
 		elif key == arcade.key.LEFT:
-			self.player.change_x = -mov_speed
+			self.player.change_x -= mov_speed
 		elif key == arcade.key.RIGHT:
-			self.player.change_x = mov_speed
+			self.player.change_x += mov_speed
 
 	def on_key_release(self, key, modifiers):
 		"""
-		Called when the user presses a mouse button.
+		Called whenever a key is released
 		"""
-		if key == arcade.key.UP or key == arcade.key.DOWN:
-			self.player.change_y = 0
-		elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
-			self.player.change_x = 0
+		if key == arcade.key.UP:
+			self.player.change_y -= mov_speed
+		elif key == arcade.key.DOWN:
+			self.player.change_y += mov_speed
+		elif key == arcade.key.LEFT:
+			self.player.change_x += mov_speed
+		elif key == arcade.key.RIGHT:
+			self.player.change_x -= mov_speed
 
 	def animate(self, dt):
 		self.allsprites.update()
